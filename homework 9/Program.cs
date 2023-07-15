9eb0d12d5006e7729acdc8e0991ca117dae11629
Maxs-B <134833723+Maxs-B@users.noreply.github.com>
2023-07-16 01:57:07 +0300

    Add files via upload

---
+++ homework 9/Program.cs
@@ -0,0 +1,59 @@
+﻿
+/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" 
+
+int n = InputNumbers("Введите N: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+/*
+ Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+ m = 2, n = 3 -> A(m,n) = 9
+ m = 3, n = 2 -> A(m,n) = 29
+ */
+/*Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m - 1, 1);
+else return akkerman(m - 1, akkerman(m, n - 1));
+}
+
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} "); */
+
+
+
+/*Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+int summa(int m, int n)
+{
+if (m==n)
+return n;
+else return m + summa(m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
